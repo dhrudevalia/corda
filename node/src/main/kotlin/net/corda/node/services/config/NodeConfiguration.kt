7fc42832c80365b99c67ae6b48c608aebc5c9557
Ryan Fowler <ryan.fowler@r3.com>
2019-06-27 13:43:46 +0100

    Code review changes - protect derivable APIs, fix global.  Also change to Duration for time rather than Long

--- node/src/main/kotlin/net/corda/node/services/config/NodeConfiguration.kt
+++ node/src/main/kotlin/net/corda/node/services/config/NodeConfiguration.kt
@@ -82,7 +82,7 @@ interface NodeConfiguration {
     val flowOverrides: FlowOverrideConfig?
 
     val cordappSignerKeyFingerprintBlacklist: List<String>
-    val cryptoServiceTimeout: Duration get() = defaultCryptoServiceTimeout
+    val cryptoServiceTimeout: Duration get() = DEFAULT_CRYPTO_SERVICE_TIMEOUT
 
     val networkParameterAcceptanceSettings: NetworkParameterAcceptanceSettings
 
@@ -100,7 +100,7 @@ interface NodeConfiguration {
 
         internal val defaultAttachmentContentCacheSize: Long = 10.MB
         internal const val defaultAttachmentCacheBound = 1024L
-        internal val defaultCryptoServiceTimeout = Duration.ofSeconds(1)
+        internal val DEFAULT_CRYPTO_SERVICE_TIMEOUT = Duration.ofSeconds(1)
 
         const val cordappDirectoriesKey = "cordappDirectories"
 
